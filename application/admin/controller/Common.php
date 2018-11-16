e18025a1dc6ea475eb9c564a5bc7e1173e54b116
wangxiangyu <261386999@qq.com>
2018-11-16 08:23:23 +0800

    11

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -14,15 +14,39 @@ use think\Session;
 class Common extends Controller
 {
     public  $id;
+    public $parme;
+
     public function __construct(Request $request = null)
     {
         $this->id = Session::get('user_id');
         $this->check();
+        $this->parme();
     }
 
     public function check(){
         if(empty($this->id)){
             return $this->redirect('part/login/checklogin');
         }
+    }
+
+    public function parme($key = '',$value = null)
+    {
+
+        $this->parme    =  input();
+        $this->parme    =   array_merge($this->parme);
+        if($key)
+        {
+            if(array_key_exists($key,$this->parme))
+            {
+                return $this->parme[$key]!=''?$this->parme[$key]:$value;
+            }else{
+                return $value!=''?$value:'';
+            }
+        }
+    }
+
+    public function __destruct()
+    {
+        // TODO: Implement __destruct() method.
     }
 }
