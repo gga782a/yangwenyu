d8a305f664b6b820fa5109319664678b18510e8d
wangxiangyu <261386999@qq.com>
2018-12-01 16:56:13 +0800

    11

--- application/admin/controller/Store.php
+++ application/admin/controller/Store.php
@@ -13,16 +13,40 @@ class Store
 {
    //生成门店二维码
 
-    public function qr_code($url='')
+    public function qr_code($url='',$level=3,$size=7)
     {
+        //dd(ROOT_PATH);
         //引入 qrcode类
         Vendor('phpqrcode.phpqrcode');
-        $qrurl = $url?$url:$_SERVER['SCRIPT_NAME'].'/admin/store/url?a=2';
-        dd(\QRcode::png($qrurl, $outfile = false, $level = 2, $size = 3, $margin = 4, $saveandprint=false) );
+        //实例化qrcode类
+        //$qrcode = new \QRcode();
+        //路径
+        $pathname = ROOT_PATH.'/public/qr_uploads/';
+        if(!is_dir($pathname)) { //若目录不存在则创建之
+            mkdir($pathname,0777,true);
+        }
+        //图片名
+        $ad = 'qrcode_' . rand(10000,99999) . '.png';
+        //图片保存路径
+        $savepath = $pathname.$ad;
+        $errorCorrectionLevel =intval($level) ;//容错级别
+        $matrixPointSize = intval($size);//生成图片大小
+        $url = 'http://www.ztwlxx.net?a=2';
+        //return $url;
+        \QRcode::png($url, $savepath, $errorCorrectionLevel, $matrixPointSize, 2);
+        return '/yangwenyu/public/qr_uploads/'.$ad;
+        //dd($png);
+        //$res = file_put_contents($savepath,$png);
+//        if($res !== false){
+//            return $savepath;
+//        }
     }
 
     public function url()
     {
-        dd(333);
+
+        $img = $this->qr_code();
+        //dd($img);
+        echo "<img src = '$img' />";
     }
 }
