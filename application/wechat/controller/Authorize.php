64c58c36c440202ff7273b3ac4267ed7d97a01c0
wangxiangyu <261386999@qq.com>
2018-12-01 09:09:37 +0800

    11

--- application/wechat/controller/Authorize.php
+++ application/wechat/controller/Authorize.php
@@ -16,7 +16,6 @@ class Authorize extends Controller
 {
     public $appId;
     public $appSecret;
-   // public $id;
     public static $table_member = 'member';
 
     public function __construct(Request $request = null)
@@ -24,12 +23,11 @@ class Authorize extends Controller
         parent::__construct($request);
         $this->appId     = _config('AppId');
         $this->appSecret = _config('AppSecret');
-        //$this->id        = Session::get('user_id');
     }
     //第一步：用户同意授权，获取code
 
     /**
-     * @param string $redirect_uri
+     * @param string $redirect_uri 授权回调地址
      * @param string $state
      */
     public function get_url($redirect_uri ='',$state='STATE')
@@ -41,7 +39,13 @@ class Authorize extends Controller
         //dd($url);
         return $this->redirect($url);
     }
-
+    //回调
+    /**
+     * @param string $code
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
     public function get_url_s($code='')
     {
         if(empty($code)){
@@ -49,26 +53,15 @@ class Authorize extends Controller
         }
         //获取网页授权token  openid
         $data = json_decode($this->get_access_token($this->appId,$this->appSecret,$code),true);
-        if($data['code'] == 200){
-            $openid = $data['data']['openid'];
-            $accesstoken = $data['data']['access_token'];
-        }else{
-            //刷新access_token
-            $accesstoken_s = json_decode($this->get_access_token_s(),true);
-            if($accesstoken_s['code'] != 200){
-                return $this->error($this->get_access_token_s());
-            }else{
-                $access_token_s = $accesstoken_s['data'];
-            }
-        }
-        $access_token=$accesstoken?$accesstoken:$access_token_s;
+
+        $openid = $data['openid'];
+
+        //获取access_token
+        $access_token = $this->get_access_token_s();
+
         //根据openid获取用户信息
-        $user = json_decode($this->get_user($access_token,$openid),true);
-        if($user['code'] != 200){
-            return $this->error($this->get_user($access_token,$openid));
-        }else{
-            $userdata = $user['data'];
-        }
+        $userdata = json_decode($this->get_user($access_token,$openid),true);
+
         $where = [
             'openid' => $userdata['openid'],
             'status' => 1,
@@ -100,6 +93,7 @@ class Authorize extends Controller
             return $this->error();
         }
     }
+    //获取网页授权token  openid
 
     /**
      * @param $appId
@@ -107,18 +101,17 @@ class Authorize extends Controller
      * @param $code
      * @return mixed
      */
-    //获取网页授权token  openid
     public function get_access_token($appId ='',$appSecret='',$code='')
     {
         $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$this->appId."&secret=".$this->appSecret."&code={$code}&grant_type=authorization_code";
         $data_token = json_decode(curl_request($url,true));
         if(array_key_exists('errcode',$data_token)){
-            return json_encode(['code'=>$data_token->errcode,'data'=>$data_token->errmsg]);
+            return $this->error(json_encode(['code'=>$data_token->errcode,'data'=>$data_token->errmsg]));
         }else{
             $gtime = 28*86400;
             cache('access_token',$data_token->access_token,7000);
             cache('refresh_token',$data_token->refresh_token,$gtime);
-            return json_encode(['code'=>'200','data'=>$data_token]);
+            return $data_token;
         }
 
     }
@@ -140,26 +133,33 @@ class Authorize extends Controller
                 $url = "https://api.weixin.qq.com/sns/oauth2/refresh_token?appid={$this->appId}&grant_type=refresh_token&refresh_token={$refresh_token}";
                 $data_token = json_decode(curl_request($url,true));
                 if(array_key_exists('errcode',$data_token)){
-                    return json_encode(['code'=>$data_token->errcode,'msg'=>$data_token->errmsg]);
+                    return $this->error(json_encode(['code'=>$data_token->errcode,'msg'=>$data_token->errmsg]));
                 }else{
                     cache('access_token',$data_token->access_token,7000);
                     $access_token = $data_token->access_token;
                 }
             }else{
-                return json_encode(['code'=>'400','msg'=>'请重新授权']);
+                return $this->error(json_encode(['code'=>'400','msg'=>'请重新授权']));
             }
         }
-        return json_encode(['code'=>'200','data'=>$access_token]);
+        return $access_token;
     }
+
     //获取用户基本信息
+
+    /**
+     * @param string $access_token
+     * @param string $openid
+     * @return mixed|void
+     */
     public function get_user($access_token='',$openid='')
     {
         $http = "https://api.weixin.qq.com/sns/userinfo?access_token={$access_token}&openid={$openid}&lang=zh_CN";
         $data = json_decode(curl_request($http));
         if(array_key_exists('errcode',$data)){
-            return json_encode(['code'=>$data->errcode,'msg'=>$data->errmsg]);
+            return $this->error(json_encode(['code'=>$data->errcode,'msg'=>$data->errmsg]));
         }else{
-            return json_encode(['code'=>'200','data'=>$data]);
+            return $data;
         }
     }
 }
